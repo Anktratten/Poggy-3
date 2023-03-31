fd4b7f536117d314132a480850d5bc76fb66b102
ArKaLbs <arvid.karlsson@elev.ga.lbs.se>
2023-03-31 12:34:08 +0200

    Typ inget lmao

--- Assets/Scripts/Arvid Based/CamTrigger.cs
+++ Assets/Scripts/Arvid Based/CamTrigger.cs
@@ -8,16 +8,20 @@ public class CamTrigger : MonoBehaviour
 
     public float PlayerX;
     public float PlayerY;
+    public float PlayerZ;
 
     public float CameraX;
     public float CameraY;
+    public float CameraZ = -10;
 
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-       collision.gameObject.transform.position = new Vector2(transform.position.x + PlayerX, transform.position.y + PlayerY);
+
+        var frog = GameObject.Find("Player");
+        frog.transform.position = new Vector3(transform.position.x + PlayerX, transform.position.y + PlayerY, transform.position.z + PlayerZ);
         var screen = GameObject.Find("Main Camera");
-        screen.transform.position = new Vector3(transform.position.x + CameraX, transform.position.y + CameraY, -10);
+        screen.transform.position = new Vector3(transform.position.x + CameraX, transform.position.y + CameraY, transform.position.z + CameraZ);
 
     }
 
