41eb529517da611b43ff2eadda5b917da0451f69
Noah <noah.dove@elev.ga.lbs.se>
2023-04-28 08:42:53 +0200

    animation moment

--- Assets/Scripts/The brit/Basic_motor_function.cs
+++ Assets/Scripts/The brit/Basic_motor_function.cs
@@ -12,10 +12,15 @@ public class Basic_motor_function : MonoBehaviour
     [HideInInspector]
     public bool LeapEnabled = false;
     public double speed;
+    Animator anim;
+    public double constant;
+    int secondDir;
     // Start is called before the first frame update
     void Start()
     {
         Goal = transform.position;
+        anim = GetComponent<Animator>();
+        anim.SetFloat("Speed", speed * constant);
     }
 
     // Update is called once per frame
@@ -23,18 +28,27 @@ public class Basic_motor_function : MonoBehaviour
     {
         transform.position = Vector2.MoveTowards(transform.position, Goal, (float)speed * Time.deltaTime);
         if (transform.position == Goal)
+        {
+            anim.SetBool("Jump", false);
+            anim.SetTrigger("StopJump");
+            if(Goal != Goal_2)
+            {
+                anim.SetBool("Jump", true);
+                anim.SetInteger("Direction", secondDir);
+            }
             Goal = Goal_2;
+        }
 
         if (Input.GetKeyDown(KeyCode.W))
-            ActionDecider(1, 0);
+            ActionDecider(1, 0, 1);
         else if (Input.GetKeyDown(KeyCode.S))
-            ActionDecider(-1, 0);
+            ActionDecider(-1, 0, 0);
         else if (Input.GetKeyDown(KeyCode.A))
-            ActionDecider(0, -1);
+            ActionDecider(0, -1, -1);
         else if (Input.GetKeyDown(KeyCode.D))
-            ActionDecider(0, 1);
+            ActionDecider(0, 1, -1);
         if (Sprinting == true)
-            ActionDecider((int)SprintGoal.y, (int)SprintGoal.x);
+            ActionDecider((int)SprintGoal.y, (int)SprintGoal.x, 2);
         
         if (Input.GetKey(KeyCode.LeftShift))
         {
@@ -46,13 +60,17 @@ public class Basic_motor_function : MonoBehaviour
             SprintGoal = new Vector2();
         }
     }
-    void ActionDecider(int y, int x)
+    void ActionDecider(int y, int x, int dir)
     {
         if (Physics2D.OverlapBox(Goal + new Vector3(x, y), transform.localScale * 0.1f, Collision))
             return;
+
+        anim.SetInteger("Direction", dir);
+        anim.SetBool("Jump", true);
         if (transform.position != Goal)
         {
             Goal_2 = Goal + new Vector3(x, y);
+            secondDir = dir;
         }
         else if (LeapEnabled == true && Input.GetKey(KeyCode.Z))
         {
