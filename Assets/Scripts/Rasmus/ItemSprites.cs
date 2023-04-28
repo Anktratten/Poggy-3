8df0cc709b0c891cd536c40a5d38020ac188389a
Anktratten <84456427+Rasmusmario123@users.noreply.github.com>
2023-04-28 11:49:36 +0200

    I FUCKING KILLED THE XP :D

--- Assets/Scripts/Rasmus/ItemSprites.cs
+++ Assets/Scripts/Rasmus/ItemSprites.cs
@@ -8,27 +8,15 @@ public class ItemSprites : MonoBehaviour
     public static Sprite[] itemSpritesArray = new Sprite[9];
     public Sprite[] shadowItemsSpritesArrayPublic = new Sprite[9];
     public static Sprite[] shadowItemsSpritesArray = new Sprite[9];
+    public Sprite[] xpBarsPublic = new Sprite[5];
+    public static Sprite[] xpBars = new Sprite[5];
     private void Start()
     {
         itemSpritesArray = itemsSpritesArrayPublic;
         shadowItemsSpritesArray = shadowItemsSpritesArrayPublic;
+        xpBars = xpBarsPublic;
     }
     public enum ItemSpritesEnum
-    {
-        carrotCoin = 0,
-        wallet = 1,
-        declaration = 2,
-        cat = 3,
-        waterBottle = 4,
-
-        vase = 5, //5
-        bible = 6, //10
-        painting = 7, //15
-
-        flowers = 8, //40
-    }
-
-    public enum ShadowItemSpritesEnum
     {
         carrotCoin = 0,
         wallet = 1,
