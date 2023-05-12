285f5a9ace03e7e81b96b9acceda6e1a46ba4409
Noah <noah.dove@elev.ga.lbs.se>
2023-05-12 12:01:40 +0200

    person collider

--- Assets/Sceneshift.cs
+++ Assets/Sceneshift.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class Sceneshift : MonoBehaviour
 {
+    public string Name;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        LoadScene(Name);
+    }
     public void LoadScene(string menuName)
     {
         SceneManager.LoadScene(menuName);
